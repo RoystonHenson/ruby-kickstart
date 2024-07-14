25f5f755c9f90ba4702f44cc1e3185f76cd600a4
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-14 14:42:40 +0100

    Code working but failing 'raise_error_'test

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -5,14 +5,40 @@
 #
 # Example:
 # two_d = [
-#   [ 1,  2,  3,  4, 5],
-#   [16, 17, 18, 19, 6],
-#   [15, 24, 25, 20, 7],
-#   [14, 23, 22, 21, 8],
-#   [13, 12, 11, 10, 9],
+#   [ 1,  2,  3,  4, 5],   
+#   [16, 17, 18, 19, 6],       
+#   [15, 24, 25, 20, 7],       
+#   [14, 23, 22, 21, 8],       
+#   [13, 12, 11, 10, 9],       
 # ]
 # order = []
 # spiral_access two_d do |i|
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+order = []
+def spiral_access(arr, &block)
+  array = arr.dup
+  sorted = []
+  sorted << array.shift
+  array = array.transpose
+  sorted << array.pop
+  until array.size == 0
+    array = array.reverse.transpose
+    sorted << array.pop
+  end
+  sorted.flatten!
+  if block
+    puts 'You invoked the block'
+    sorted.map(&block)
+ else
+   sorted
+  end
+end
+=begin
+ a = [[ 1,  2,  3,  4, 5],       
+          [16, 17, 18, 19, 6],       
+          [15, 24, 25, 20, 7],       
+          [14, 23, 22, 21, 8],       
+          [13, 12, 11, 10, 9]]
+=end
